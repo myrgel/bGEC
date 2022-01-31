6b4064bad654637964f45e55b89d7616ada039d9
martinpedak <nizu08@gmail.com>
2022-01-31 18:55:19 +0200

    init 3

--- bGEC/subword_tokenizer/subword_tokenizer.py
+++ bGEC/subword_tokenizer/subword_tokenizer.py
@@ -14,3 +14,14 @@ import tensorflow_datasets as tfds
 import tensorflow_text as text
 import tensorflow as tf
 
+tf.get_logger().setLevel('ERROR')
+pwd = pathlib.Path.cwd()
+
+examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
+                               as_supervised=True)
+train_examples, val_examples = examples['train'], examples['validation']
+
+
+for pt, en in train_examples.take(1):
+  print("Portuguese: ", pt.numpy().decode('utf-8'))
+  print("English:   ", en.numpy().decode('utf-8'))
